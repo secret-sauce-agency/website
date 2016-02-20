f08c78e89b79d69fff49012efba7bea82556c339
Taylor Van Orden <tayvano@gmail.com>
2016-02-20 12:29:34 -0800

    tons more updates

---
+++ js/secretsauce-master.js
@@ -0,0 +1,19 @@
+  // Smooth Scolling - All Pages
+  //https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  // needs jquery <1.12.0
+  $(function() {
+    $("a[href*=#]:not([href=#])").click(function() {
+        if (location.pathname.replace(/^\//,") == this.pathname.replace(/^\//,")
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $("[name=" + this.hash.slice(1) +"]");
+               if (target.length) {
+                 $("html,body").animate({
+                     scrollTop: target.offset().top
+                }, 400);
+                return false;
+            }
+        }
+      });
+  });
