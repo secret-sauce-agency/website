b8854e0bfd1deec25eb7e4e002ce84175cd5928b
George Song <george@monozuku.com>
2016-03-02 09:47:39 -0800

    Clean up JS

--- js/secretsauce-master.js
+++ js/secretsauce-master.js
@@ -1,19 +1,22 @@
-  // Smooth Scolling - All Pages
-  //https://css-tricks.com/snippets/jquery/smooth-scrolling/
-  // needs jquery <1.12.0
-  $(function() {
-    $("a[href*=#]:not([href=#])").click(function() {
-        if (location.pathname.replace(/^\//,") == this.pathname.replace(/^\//,")
-            || location.hostname == this.hostname) {
+// Smooth Scolling - All Pages
+//https://css-tricks.com/snippets/jquery/smooth-scrolling/
+// needs jquery <1.12.0
+$(function() {
 
-            var target = $(this.hash);
-            target = target.length ? target : $("[name=" + this.hash.slice(1) +"]");
-               if (target.length) {
-                 $("html,body").animate({
-                     scrollTop: target.offset().top
-                }, 400);
-                return false;
-            }
-        }
-      });
-  });
+$("a[href*=#]:not([href=#])").click(function() {
+  if (
+    location.pathname.replace(/^\//,") == this.pathname.replace(/^\//,") ||
+    location.hostname == this.hostname
+  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) +"]");
+    if (target.length) {
+      $("html,body").animate({
+        scrollTop: target.offset().top
+      }, 400);
+      return false;
+    }
+  }
+});
+
+});
